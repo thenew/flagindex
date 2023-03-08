b9424111118eba7414cbeb011cb00716d72f4b40
thenew <remy@thenew.fr>
2023-03-08 23:03:11 +0100

    Update vercel config

--- api/index.js
+++ api/index.js
@@ -1,6 +1,12 @@
 // https://vercel.com/guides/using-express-with-vercel
 const app = require('express')();
 
+app.get('/', (req, res) => {
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.end(`Hello W!`);
+});
+
 app.get('/api', (req, res) => {
   const path = `/api/item/1234`;
   res.setHeader('Content-Type', 'text/html');
@@ -11,6 +17,11 @@ app.get('/api', (req, res) => {
 app.get('/api/item/:slug', (req, res) => {
   const { slug } = req.params;
   res.end(`Item: ${slug}`);
+});
+
+const port = 4200;
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
 });
 
 module.exports = app;
